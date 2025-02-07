26f532394404560763e0091c9a217a5aa655b39b
RedGnom <arsik172004@mail.ru>
2025-02-07 23:14:24 +0500

    Добалвения обработки ввода в библиотеку

---
+++ LAB10/LAB10_lib/Interface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB10_lib
+{
+    public class Interface
+    {
+        public static int ReadElem()
+        {
+            int n;
+            bool isCorrect = true;
+            do
+            {
+                isCorrect = int.TryParse(Console.ReadLine(), out n);
+                if (!isCorrect || n < 0)
+                {
+                    Console.WriteLine("Неверные данные, повторите попытку ввода");
+                    isCorrect = false;
+                }
+            }
+            while (!isCorrect);
+            return n;
+        }
+    }
+}
